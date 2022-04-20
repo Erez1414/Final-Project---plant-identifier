e948806372278f3044758043c42eabd716394072
Erez1414 <80767362+Erez1414@users.noreply.github.com>
2022-04-20 16:39:46 +0300

    Add files via upload

---
+++ Server-Flask.py
@@ -0,0 +1,121 @@
+import flask
+import werkzeug
+from flask import Flask, request
+from flask_restful import Api, Resource
+import pandas as pd
+import numpy as np
+import tensorflow as tf
+import os
+
+config = tf.compat.v1.ConfigProto()
+config.gpu_options.allow_growth = True
+sess = tf.compat.v1.Session(config=config)
+df = pd.read_csv("plants_info.csv")
+df.set_index("type", inplace=True)
+print("Loading model")
+# loaded_model = tf.saved_model.load("model.pb")  # with save
+# loaded_model = tf.keras.Sequential([hub.KerasLayer("model.pb",trainable=False),
+#                             tf.keras.layers.Dense(512, activation='relu'),
+#                             tf.keras.layers.Dense(5, activation='softmax')])
+# loaded_model.build([None, 224, 224, 3])
+
+
+
+
+loaded_model = tf.keras.models.load_model(
+    "mobileNet_model"+ os.sep +"inaturalist1640537367")#"model.pb")
+labels = np.load("labels.npy")
+print(labels)
+
+# print(labels)
+# print("model loaded")
+# print(type(loaded_model))
+
+app = Flask(__name__)
+api = Api(app)
+pd.options.display.max_colwidth = 1000
+
+def get_data_from_name(name):
+    if name not in df.index:
+        return dict()
+    plant_info = df.loc[name]
+    # print("fip",plant_info,"fin",sep='\n')
+    plant_info_to_send = plant_info.to_string(header=False, index=False)
+    # plant_info_to_send = plant_info_to_send.replace(" ", "")
+    plant_info_to_send = plant_info_to_send.split("\n")
+    # print(plant_info_to_send)
+    # print(plant_info_to_send[3])
+    return {"type": name,
+            "desc": plant_info_to_send[0], #.replace(" ",""),
+            "water": plant_info_to_send[1], #.replace(" ",""),
+            "sun": plant_info_to_send[2], #.replace(" ",""),
+            "soil": plant_info_to_send[3],
+            "alarm": plant_info_to_send[4].replace(" ","")}
+
+
+@app.route('/search/<string:searchKey>', methods=['GET', 'POST'])
+def handle_search_request(searchKey):
+    if request.method == 'GET':
+        return get_data_from_name(searchKey)
+    else:
+        return "Problem on server side"
+
+
+
+def prep_image(img):
+    img_height = 224
+    img_width = 224
+    pic = tf.keras.utils.image_dataset_from_directory(img,
+                                                      # labels=None,
+                                                      batch_size=1,
+                                                      image_size=
+                                                      (img_height, img_width)
+                                                      )
+
+    normalization_layer = tf.keras.layers.Rescaling(1. / 255)
+    pic = pic.map(
+        lambda x, y: (normalization_layer(x), y))
+
+    for test_batch, test_labels_batch in pic:
+        print(test_batch.shape)
+        print(test_labels_batch.shape)
+        break
+
+    predicted_test_labeld = loaded_model.predict(test_batch)
+    # print(predicted_test_labeld)
+    ind = np.argmax(predicted_test_labeld[0])
+    return labels[ind]
+
+# def get_label(img):
+#     predicted_odds = loaded_model.predict(numpy.array([img]))[0]
+#     print(predicted_odds, labels, sep='\n')
+#     # predicted_odds = np.abs(predicted_odds)
+#     ind = predicted_odds.argmax(axis=0)
+#     return labels[ind]
+
+@app.route('/recognize/', methods=['GET', 'POST'])
+def handle_recognize_request():
+    if request.method == 'POST':
+        imagefile = flask.request.files['image']
+        filename = werkzeug.utils.secure_filename(imagefile.filename)
+        print("\nReceived image File name : " + imagefile.filename)
+        imagefile.save("to_predict" + os.sep + "place_holder"+ os.sep
+                       + filename) # was
+        # just "
+                       # "filename
+        # img = imageio.imread(filename)
+        # img = process_image(img)
+        # imagefile.save(imageio.imwrite("img.jpg",img))
+        img_lbl = prep_image("to_predict")
+        # predicted_label = get_label(img_lbl)
+        print(img_lbl)
+
+        return get_data_from_name(img_lbl)
+    else:
+        return 'problem happened on server side'
+
+# api.add_resource(Search, "/search/<string:searchKey>")
+
+if __name__ == "__main__":
+    app.run(debug=False, host='0.0.0.0') # for connecting from WAN (dont
+    # forget to open ports on router!!!
---
+++ img_preprocess.py
@@ -0,0 +1,119 @@
+'''
+Image preprocessing for CNN.
+Input: jpg files exported from Mac Photo app
+Output: Saved numpy array of images in specified shape for use in CNN. Corrects for class imbalance via image generation.
+Images are resized to specified shape, cropped to square, then image generation is used to create flipped/rotated images.
+'''
+
+from __future__ import print_function
+import numpy as np
+import pandas as pd
+import os, cv2, re, PIL
+from os import listdir
+from os.path import isfile, join
+from PIL import Image
+from skimage import io
+from skimage.transform import resize
+
+from src.img_resize import my_image_resize
+from src.img_resize import my_image_rename
+
+np.random.seed(1337)  # for reproducibility
+
+
+def image_categories(image_root):
+    ''' A dictionary that stores the image path name and flower species for each image
+    Input: image path names (from root directory)
+    Output: dictionary 'categories'
+    '''
+    flower_dict = {}
+    for dirname in listdir(image_root):
+        img_root = image_root + dirname + os.sep
+        print("heree0", img_root)
+        files = [f for f in listdir(img_root) if isfile(join(img_root, f))]
+        # for path, subdirs, files in os.walk(resized_root):
+            # print(path, subdirs, files)
+        for name in files:
+            print("here1", name)
+            # name = name.replace(' ', '')
+            # name = name.replace('-', '_')
+            if not (name.startswith('.')):
+            #     if name != 'cnn_capstone.py':
+                img_path = '{}{}'.format(img_root, name)
+                print("hereere2 ", img_path)
+                # img_path = os.path.join(path, name)
+                # img_cat = re.sub("\d+", "", name).rstrip('_.jpg')
+                # img_cat = img_cat[:-3]
+                flower_dict[img_path] = dirname
+    return flower_dict
+
+def _center_image(img, new_size=[256, 256]):
+    '''
+    Helper function. Takes rectangular image resized to be max length on at least one side and centers it in a black square.
+    Input: Image (usually rectangular - if square, this function is not needed).
+    Output: Image, centered in square of given size with black empty space (if rectangular).
+    '''
+    row_buffer = (new_size[0] - img.shape[0]) // 2
+    col_buffer = (new_size[1] - img.shape[1]) // 2
+    centered = np.zeros(new_size + [img.shape[2]], dtype=np.uint8)
+    centered[row_buffer:(row_buffer + img.shape[0]), col_buffer:(col_buffer + img.shape[1])] = img
+    return centered
+
+def resize_image_to_square(img, new_size=((256, 256))):
+    '''
+    Resizes images without changing aspect ratio. Centers image in square black box.
+    Input: Image, desired new size (new_size = [height, width]))
+    Output: Resized image, centered in black box with dimensions new_size
+    '''
+    if(img.shape[0] > img.shape[1]):
+        tile_size = (int(img.shape[1]*new_size[1]/img.shape[0]),new_size[1])
+    else:
+        tile_size = (new_size[1], int(img.shape[0]*new_size[1]/img.shape[1]))
+    # print(cv2.resize(img, dsize=tile_size))
+    return _center_image(cv2.resize(img, dsize=tile_size), new_size)
+
+def crop_image(img, crop_size):
+    '''
+    Crops image to new_dims, centering image in frame.
+    Input: Image, desired cropped size (crop_size=[height, width])
+    Output: Cropped image
+    '''
+    row_buffer = (img.shape[0] - crop_size[0]) // 2
+    col_buffer = (img.shape[1] - crop_size[1]) // 2
+    return img[row_buffer:(img.shape[0] - row_buffer), col_buffer:(img.shape[1] - col_buffer)]
+
+def process_images(file_paths_list, resize_new_size=[256,256], crop_size=[224, 224]):
+    '''
+    Input: list of file paths (images)
+    Output: numpy array of processed images: normalized, resized, centered)
+    '''
+    x = []
+
+    for file_path in file_paths_list:
+        img = cv2.imread(file_path)
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        img = resize_image_to_square(img, new_size=resize_new_size)
+        img = crop_image(img, crop_size=crop_size)
+        x.append(img)
+    x = np.array(x)
+    return x
+
+def process_image(img, resize_new_size=[256,256], crop_size=[224, 224]):
+    # img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    img = resize_image_to_square(img, new_size=resize_new_size)
+    img = crop_image(img, crop_size=crop_size)
+    return img
+
+
+if __name__ == '__main__':
+    img_root = '../imgs_jpgs/'
+    # Rename files exported from Mac Photo app (in place)
+    my_image_rename(img_root)
+    # Create y (labels) and file list (x) from image names
+    y_dict = image_categories(img_root)
+    y = list(y_dict.values())
+    file_list = list(y_dict.keys())
+    # with Pool(4) as p:
+    #     p.map(process_images(file_list, resize_new_size=[256,256], crop_size=[224, 224]), file_list)
+    image_array = process_images(file_list, resize_new_size=[256,256], crop_size=[224, 224])
+    np.savez('flowers_224.npz', image_array, y)
